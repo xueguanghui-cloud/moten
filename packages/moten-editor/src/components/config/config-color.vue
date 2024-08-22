871bf06eca60e872eb6f5e4a8df95bd9764cba75
codexgh991124 <codexgh@163.com>
2024-08-22 22:39:55 +0800

    feat(moten-editor): 页面颜色配置

---
+++ packages/moten-editor/src/components/config/config-color.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { ref, toRefs, watch } from 'vue'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: () => {},
+  },
+  viewport: {
+    type: String,
+    default: 'desktop',
+  },
+})
+
+const emit = defineEmits(['callback', 'update'])
+
+const { data } = toRefs(props)
+const { formData, key, id } = data.value
+const { title, default: defaultValue } = data.value.properties[props.viewport]
+const input = ref('')
+
+watch(
+  () => formData,
+  (value) => {
+    input.value = value?.[props.viewport] || defaultValue
+  },
+  {
+    immediate: true,
+  },
+)
+
+watch(
+  input,
+  (value) => {
+    let data = {}
+    const _value = value || ''
+    if (Object.values(formData || {}).length < 2) {
+      data = { desktop: _value, mobile: _value }
+    } else {
+      data = { [props.viewport]: _value }
+    }
+
+    emit('callback', {
+      data: {
+        [key]: data,
+      },
+      id,
+    })
+    emit('update', {
+      [key]: data,
+    })
+  },
+  { immediate: true },
+)
+</script>
+
+<template>
+  <div class="config-color">
+    <el-form-item :label="title" :prop="key + '.' + viewport">
+      <el-color-picker v-model="input"  />
+    </el-form-item>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.config-color {
+  position: relative;
+}
+</style>
